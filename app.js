cd8c02135170e419320788423b76085ad1fec8f9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-28 02:41:26 +0900

    Use co-body to parse request body

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var querystring = require('querystring');
 
 var koa    = require('koa');
 var route  = require('koa-route');
+var parse  = require('co-body');
 
 var chatwork = require('./lib/chatwork');
 var client   = chatwork(process.env.CHATWORK_API_TOKEN);
@@ -10,24 +11,16 @@ var PORT     = process.env.PORT || 5000;
 var app = koa();
 
 app.use(route.post('/chatwork/:room_id', function *(room_id) {
-  var body = ''
+  var params   = yield parse(this.req);
+  var payload  = JSON.parse(params.payload);
+  var endpoint = 'rooms/' + room_id + '/messages';
+  var message  = [
+    '(bot)',
+    '@' + payload.pusher.name + 'さんが push しました:',
+    '> ' + payload.compare
+  ].join('\n');
 
-  this.req.on('data', function(chunk) {
-    body += chunk;
-  });
-
-  this.req.on('end', function() {
-    var params   = querystring.decode(body);
-    var payload  = JSON.parse(params.payload);
-    var endpoint = 'rooms/' + room_id + '/messages';
-    var message = [
-      '(bot)',
-      '@' + payload.pusher.name + 'さんが push しました:',
-      '> ' + payload.compare
-    ].join('\n');
-
-    client.post(endpoint , {body: message});
-  });
+  client.post(endpoint , {body: message});
 
   this.body = 'OK';
 }));
