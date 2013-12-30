3a4a809565552a4686e93c5f5b367b0695794b14
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-31 01:29:15 +0900

    Remove unnecessary newline

--- app.js
+++ app.js
@@ -14,16 +14,18 @@ app.use(route.post('/chatwork/:room_id', function *(room_id) {
   var params   = yield parse(this);
   var payload  = JSON.parse(params.payload);
   var endpoint = 'rooms/' + room_id + '/messages';
-  var branch   = payload.ref.replace(/^refs\/heads\//, '');
+
+  var branch         = payload.ref.replace(/^refs\/heads\//, '');
+  var commitMessages = payload.commits.map(function(commit) {
+      // return commit.message + ' ' + commit.url; // chatwork で見栄えが悪いので一旦コメントアウト
+      return '* ' + commit.message;
+    }).join('\n');
+
   var message  = [
     '[info][title]' + '@' + payload.pusher.name + 'さんが push しました' + '[/title]',
     'ブランチ: ' + branch + '( ' + payload.repository.url + '/tree/' + branch + ' )',
     '内容を見る: ' + payload.compare,
-    '[hr]',
-    payload.commits.map(function(commit) {
-      // return commit.message + ' ' + commit.url; // chatwork で見栄えが悪いので一旦コメントアウト
-      return '* ' + commit.message;
-    }).join('\n'),
+    '[hr]' + commitMessages,
     '[/info]'
   ].join('\n');
 
